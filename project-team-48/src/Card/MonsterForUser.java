a1f19694757715e7b0d27f4555b6c09574ebc0d8
amirhosseinkhb <amirhossein.khoshb@gmail.com>
2021-05-04 15:30:42 +0430

    .

--- project-team-48/src/Card/MonsterForUser.java
+++ project-team-48/src/Card/MonsterForUser.java
@@ -13,7 +13,7 @@ class MonsterForUser<User> extends MonsterCard {
     public Field field;
 
     public MonsterForUser(MonsterCard monsterCard, User user) {
-super(monsterCard.name, monsterCard.description, monsterCard.price, monsterCard.level, monsterCard.type, monsterCard.ATK, monsterCard.DEF,monsterCard.);
+super(monsterCard.name, monsterCard.description, monsterCard.price, monsterCard.level, monsterCard.type, monsterCard.ATK, monsterCard.DEF,monsterCard.cardType);
         this.user = user;
     }
 
