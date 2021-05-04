b6139052a9fe2a142a6981396362c7c2e38129ba
amirhosseinkhb <amirhossein.khoshb@gmail.com>
2021-05-04 10:39:44 +0430

    .

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class Main{
    public static void main(String [] args)
    {
-      /////////////////////////////////////
+      ////////////////kkkkkkkkkkkkk
       new ProgramControler();
    }
 }
