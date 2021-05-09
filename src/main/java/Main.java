3603d88058a5c58ae78599eea0737010aff22002
amirhosseinkhb <amirhossein.khoshb@gmail.com>
2021-05-09 21:49:05 +0430

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import Card.CartReader;
 
 public class Main {
-    /////////////////////////////////
+
     public static void main(String[] args) {
         new CartReader();
         new ProgramController();
