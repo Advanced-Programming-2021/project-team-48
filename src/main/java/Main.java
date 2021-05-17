95ef53fe21aab2f852e1596bd82d00c7017b64e1
nimamonaghesh <nima.monaghesh@gmail.com>
2021-05-17 11:27:07 +0430

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        ArrayList<User> users = new ArrayList<>();
-        users.add(new User("project","project","project"));
-        users.add(new User("46","46","46"));
-        users.add(new User("ok","ok","ok"));
-
-        System.out.println(users);
-
-        try {
-            FileWriter myWriter = new FileWriter("monsterCards.json");
-            myWriter.write(new Gson().toJson(users.toArray()));
-            myWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
 //        new CartReader();
         new ProgramController();
     }
